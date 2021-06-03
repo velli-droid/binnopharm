fb6462422b0228490775a9dc46ce022ba95622eb
Valeriy Boyariniv <vel.boyar@gmail.com>
2021-06-03 09:51:39 +0300

    Переделал слайдер, добавил ссылки

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ const searchForm = document.querySelector('.header__search');
 const searchBtn = document.querySelector('.search-btn');
 const searchBtnclose = searchForm.querySelector('.search-form__close');
 
-searchBtn.onclick = function(e) {
+searchBtn.onclick = function (e) {
     e.preventDefault();
     searchBtn.classList.toggle('active');
     searchForm.classList.toggle('active');
 }
 
-searchBtnclose.onclick = function()  {
+searchBtnclose.onclick = function () {
     searchBtn.classList.toggle('active');
     searchForm.classList.toggle('active');
 }
@@ -17,7 +17,7 @@ searchBtnclose.onclick = function()  {
 
 
 // swipers
-const gradientSwiperContainer =  document.querySelector('.gradient-swiper');
+const gradientSwiperContainer = document.querySelector('.gradient-swiper');
 const verticalSwiperContainer = document.querySelector('.production-swiper');
 const newsSwiperContainer = document.querySelector('.news-swiper__container');
 
@@ -67,49 +67,57 @@ ymaps.ready(init);
 
 function init() {
     const myMap = new ymaps.Map("map", {
-        center: [55.72841212167653,37.64593570051657],
-        zoom: 18,
+        center: [55.72841212167653, 37.64593570051657],
+        zoom: 11,
         controls: [],
     }, {
         yandexMapDisablePoiInteractivity: true
     });
+
+    var polygon = new ymaps.Polygon([
+        [
+            [55.72791171815129,37.64554206450491],
+            [55.7278333870453,37.64587063510899],
+            [55.72797713006052,37.64597763281463],
+            [55.72799983466117,37.646000431591155],
+            [55.728013835825166,37.64602993589022],
+            [55.728024052887406,37.646074192338624],
+            [55.72802367447801,37.646127165967094],
+            [55.72799226646256,37.64628943961254],
+            [55.72837503248383,37.6465702368671],
+            [55.72844201033238,37.64656889576276],
+            [55.72845866083172,37.64649915832948],
+            [55.728466607345055,37.646420703715805],
+            [55.728463201696684,37.6463469429679],
+            [55.72845147112778,37.64627519387675],
+            [55.728429145196536,37.646210820860404],
+            [55.72841552258813,37.646182657665726],
+            [55.72836935704633,37.64611694354486],
+            [55.7283178937548,37.64608743924572],
+            [55.72826983606077,37.646078722066406],
+            [55.728243725950115,37.64606061715556],
+            [55.72825886224829,37.64599423248245],
+            [55.72816653073783,37.64592583615255],
+            [55.7281922624923,37.645813183373946],
+            [55.72819756020435,37.645705224461096],
+            [55.727980342964045,37.645543584315966],
+            [55.72791171815129,37.64554206450491]
+        ]
+    ], {
+        hintContent: `<div class="map__hint">ул. Кожевническая, д. 14, стр. 5</div>`,
+        balloonContent: [
+            '<div class="map__balloon">',
+            '<img class="map__logo" src="img/logo-blue.png" alt="Лого"/>',
+            '</div>'
+        ].join('')
+    }, {
+        fillColor: '#0e65c8b2',
+        interactivityModel: 'default#transparent',
+        strokeWidth: 1,
+        opacity: 1,
+    });
+
+    myMap.geoObjects.add(polygon);
+    myMap.setBounds(polygon.geometry.getBounds());
+
 }
-
-const polygon = new YMaps.Polygon([ new YMaps.GeoPoint(55.727912004872245,37.64554200808256),
-    new YMaps.GeoPoint(55.727978605108326,37.64554200808256),
-    new YMaps.GeoPoint(55.72819770394362,37.64570629338471),
-    new YMaps.GeoPoint(55.72819202782357,37.6458108995363),
-    new YMaps.GeoPoint(55.72816553925239,37.64592623452395),
-    new YMaps.GeoPoint(55.72825938085115,37.645993571385716),
-    new YMaps.GeoPoint(55.72824386614566,37.646061297163314),
-    new YMaps.GeoPoint(55.72826830665043,37.64607955922514),
-    new YMaps.GeoPoint(55.72831447231222,37.64608559419544),
-    new YMaps.GeoPoint(55.728353448197105,37.64610571076305),
-    new YMaps.GeoPoint(55.72836707082727,37.64611308683784),
-    new YMaps.GeoPoint(55.72839923535178,37.646155331629835),
-    new YMaps.GeoPoint(55.72842685898098,37.64620897581012),
-    new YMaps.GeoPoint(55.72845221215765,37.64627737214),
-    new YMaps.GeoPoint(55.72846242910476,37.64634710957439),
-    new YMaps.GeoPoint(55.7284662314998,37.6464235544559),
-    new YMaps.GeoPoint(55.72845790658095,37.64650133851733),
-    new YMaps.GeoPoint(55.72844125673789,37.64656906429496),
-    new YMaps.GeoPoint(55.72837390048184,37.646569734847205),
-    new YMaps.GeoPoint(55.7279919240395,37.64628937795239),
-    new YMaps.GeoPoint(55.72802408887438,37.64612442209797),
-    new YMaps.GeoPoint(55.72802408887438,37.64607413067894),
-    new YMaps.GeoPoint(55.728013114992514,37.646027192021165),
-    new YMaps.GeoPoint(55.728001005877914,37.6460037226923),
-    new YMaps.GeoPoint(55.7279790580981,37.64597824170666),
-    new YMaps.GeoPoint(55.72783374834668,37.64586961224156),
-    new YMaps.GeoPoint(55.727912004872245,37.64554200808256),
-]);
-const style = new YMaps.Style();
-style.polygonStyle = new YMaps.PolygonStyle();
-style.polygonStyle.fill = true;
-style.polygonStyle.outline = flase;
-style.polygonStyle.strokeWidth = 1;
-style.polygonStyle.fillColor = "5693d9";
-
-polygon.setStyle(style);
-map.addOverlay(polygon);
-
